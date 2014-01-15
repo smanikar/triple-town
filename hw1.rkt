af4ef057ded1adf9307251e82f1eb391422674b5
Srikanth Manikarnike <srikanth.m13@gmail.com>
2014-01-14 18:07:47 -0700

    next-tile working

--- hw1.rkt
+++ hw1.rkt
@@ -1,1 +1,30 @@
 #lang racket
+(require rackunit)
+
+; A tile is either
+  ;   - 'blank
+  ;   - 'grass
+  ;   - 'bush
+  ;   - 'tree
+  ;   - 'shack
+  ;   - 'house
+  ;   - 'mansion
+  ;   - 'castle
+
+; next-tile : string -> string
+; Gets the next tile of cur-tile
+(define (next-tile cur-tile)
+  (cond
+    [(symbol=? cur-tile 'blank) 'grass]
+    [(symbol=? cur-tile 'grass) 'bush]
+    [(symbol=? cur-tile 'bush)  'tree]
+    [(symbol=? cur-tile 'tree)  'shack]
+    [(symbol=? cur-tile 'shack) 'house]
+    [(symbol=? cur-tile 'house) 'mansion]
+    [(symbol=? cur-tile 'mansion) 'castle]
+    [else 'bad-input]
+    ))
+
+(module+ test 
+  (check-equal? (next-tile 'blank) 'grass)
+  (check-equal? (next-tile 'test) 'bad-input))
