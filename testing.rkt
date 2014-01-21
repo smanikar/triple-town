cbb3f9eb717e8f1281941f9baa674bafdcd39261
Srikanth Manikarnike <srikanth.m13@gmail.com>
2014-01-21 14:12:43 -0700

    Make board.rkt presentable. Backup changes in testing.rkt

--- testing.rkt
+++ testing.rkt
@@ -2,15 +2,18 @@
 (require rackunit)
 (require test-engine/racket-tests)
 
+; Task for 1/22
+; function that takes a move and put it on the board and makes a collapse
+
 ; A tile is either
-;   - 'blank 
-;   - 'grass
-;   - 'bush
-;   - 'tree
-;   - 'shack
-;   - 'house
-;   - 'mansion
-;   - 'castle
+  ;   - 'blank 
+  ;   - 'grass
+  ;   - 'bush
+  ;   - 'tree
+  ;   - 'shack
+  ;   - 'house
+  ;   - 'mansion
+  ;   - 'castle
 
 ; next-tile : symbol -> symbol
 ; Gets the next tile of cur-tile
@@ -35,12 +38,18 @@
   (check-error (next-tile 'test) "not a tile: test")
   )
 
-;Board-size = 3
-(define board-size 3)
 
 ; A tile is
 ;  (make-tile symbol num num boolean)
 (define-struct tile ((val) x y (chk #:mutable)) #:transparent)
+
+(define game-board
+  (list
+   (list (tile 'grass 0 0 #t) (tile 'blank 1 0 #t) (tile 'grass 2 0 #f))
+   (list (tile 'blank 0 1 #t) (tile 'grass 1 1 #f) (tile 'blank 2 1 #f))
+   (list (tile 'grass 0 2 #f) (tile 'blank 1 2 #f) (tile 'grass 2 2 #f))))
+
+(define board-size (length game-board))
 
 (define board1
   (list
@@ -81,12 +90,12 @@
           (and (< y board-size) (> y -1))) #t]
     [else #f]))
 
-;(module+ test
-;  (check-equal? (on-board? empty  1 0) #f)
-;  (check-equal? (on-board? board1  1 0) #t)
-;  (check-equal? (on-board? board1a 1 8) #f)
-;  (check-equal? (on-board? board1  -1 0) #f)
-;  )
+(module+ test
+  (check-equal? (on-board? empty  1 0) #f)
+  (check-equal? (on-board? board1  1 0) #t)
+  (check-equal? (on-board? board1a 1 8) #f)
+  (check-equal? (on-board? board1  -1 0) #f)
+  )
 
 ; replace-neighbour-row list-of-tiles num num -> list-of-tiles
 ; Find and replace all visited tiles with blank except ('x', 'y')
@@ -99,6 +108,7 @@
        [(tile-chk (first row)) 
         (cond 
           [(and (equal? (tile-x (first row)) x)
+                (equal? (tile-y (first row)) y)
                 (equal? (tile-y (first row)) y))
            (list* (tile (next-tile (tile-val (first row))) x y #t)
                   (replace-neighbour-row (rest row) x y))]
@@ -147,21 +157,28 @@
            (replace-neighbour (rest board) x y))]))
 
 (module+ test 
-  (check-equal? (replace-neighbour board1 0 0) board1a)
+  ;(check-equal? (replace-neighbour board1 0 0) board1a)
   ;(check-equal? (replace-neighbour board1 1 0) board1)
+  (check-equal? (replace-neighbour 
+                 (list (list (tile 'grass 0 0 #t) (tile 'grass 1 0 #t))
+                       (list (tile 'grass 0 1 #t) (tile 'blank 1 1 #f))) 0 0)
+                (list (list (tile 'bush  0 0 #t) (tile 'blank 1 0 #t))
+                      (list (tile 'blank 0 1 #t) (tile 'blank 1 1 #f))))
   )
 
 ; valid-neighbour? : list-of-list-of-tile num num symbol -> boolean
 ; Checks if a given tile is a valid neighbour based on 'val'
 (define (valid-neighbour? board x y val)
   (if (on-board? board x y)
-      (if (and (symbol=? (tile-val(get-tile board x y)) val)
-               (not (tile-chk (get-tile board x y))))
+      (if (and (symbol=? (tile-val (get-tile board x y)) val)
+               (not (tile-chk (get-tile board x y)))
+               (not (symbol=? (tile-val (get-tile board x y)) 'blank)))
           #t #f)
       #f))
 
 ;count-neighbour :  list-of-list-of-tile num num num -> num
-;Returns 'count', the number of relevant neighbouring tiles of ('x','y') that are same
+;Returns 'count', the number of relevant neighbouring tiles of ('x','y') 
+; that are same
 ;Algo-
 ;Increment count
 ;Mark (x,y) as visited
@@ -203,12 +220,85 @@
        [else c])]))
  
 
+; reset-check-row : list-of-tiles -> list-of-tiles
+; Resets all check fields in 'row' to #f
+
+(define (reset-check-row row)
+  (cond
+    [(empty? row) row]
+    [(cons? row) (cons (tile (tile-val (first row))
+                             (tile-x (first row))
+                             (tile-y (first row))
+                             #f)
+                       (reset-check-row (rest row)))]))
+
+(module+ test
+  (check-equal? (reset-check-row (cons (tile 'grass 0 0 #t) empty))
+               (cons (tile 'grass 0 0 #f) empty))
+  (check-equal? (reset-check-row (list (tile 'grass 0 0 #t) 
+                                      (tile 'grass 0 0 #t)
+                                      (tile 'grass 0 0 #t)))
+               (list (tile 'grass 0 0 #f) 
+                     (tile 'grass 0 0 #f)
+                     (tile 'grass 0 0 #f))))
+
+; reset-check : list of list of tiles -> list of list of tiles
+; Resets all check fields in 'board' to #f
+  
+(define (reset-check board)
+  (cond
+    [(empty? board) board]
+    [(cons? board) (cons (reset-check-row (first board))
+                         (reset-check (rest board)))]))
+
 (module+ test 
-  ;(check-equal? (count-neighbour empty 0 0 0) 0)
-  ;(check-equal? (count-neighbour (cons (cons (tile 'grass 0 0 #f) empty) empty) 0 0 0) 1)
-;  (check-equal? (count-neighbour (list (list (tile 'grass 0 0 #f) (tile 'grass 1 0 #f))
-;                                        (list (tile 'blank 0 1 #f) (tile 'blank 1 1 #f)))
-;                                 0 0 0) 2)
-    (check-equal? (count-neighbour board1b 0 0 0) 3)
-    (check-equal? (count-neighbour board1 1 0 0) 1)
+  (check-equal? (reset-check empty) empty)
+  (check-equal? (reset-check 
+                 (cons (cons (tile 'grass 0 0 #t) empty) empty)) 
+                (cons (cons (tile 'grass 0 0 #f) empty) empty))
+  (check-equal? (reset-check 
+                 (list (list (tile 'grass 0 0 #t) (tile 'grass 1 0 #f))
+                       (list (tile 'blank 0 1 #f) (tile 'blank 1 1 #t))))
+                (list (list (tile 'grass 0 0 #f) (tile 'grass 1 0 #f))
+                      (list (tile 'blank 0 1 #f) (tile 'blank 1 1 #f))))
   )
+
+; collapse-board : list-of-list-of-tile -> void
+; Collapse all possible 3 or more occurances of 'val' into next 'val' in 'board'
+
+(define (collapse-board)
+  (for-each 
+   (lambda (lot)
+     (for-each
+      (lambda (tile)
+        (set! game-board (reset-check game-board))
+        (if (not (symbol=? (tile-val tile) 'blank))
+            (if (> (count-neighbour game-board
+                                    (tile-x tile)
+                                    (tile-y tile)
+                                    0)
+                   2)
+                (set! game-board 
+                      (replace-neighbour game-board (tile-x tile) (tile-y tile)))
+                empty)
+            empty))
+      lot))
+   game-board))
+
+(collapse-board)
+
+;collapse-tile : list-of-list-of-tile num num -> list-of-list-of-tile
+;Collapses all neighbours of ('x','y') based on 'val' of 'tile'('x','y') in 'board'
+
+;Algo-
+;check all neighbours of xy
+;if xy is blank, return board
+;if (> 1)neighbours are the same as xy, replace them with blank
+;   and replace xy with next-tile
+;else return board
+
+;(define (collapse-tile board x y)
+;  (cond
+;    [(symbol=? (tile-val (get-tile board x y)) 'blank) board]
+;    [(> (count-neighbour board x y 0) 1) (replace-neighbour board x y)]
+;    [else board]))
