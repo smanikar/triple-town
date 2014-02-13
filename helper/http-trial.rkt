ef8fd0407013f7592042c1e3d42ce1fd385e4c2d
Srikanth Manikarnike <srikanth.m13@gmail.com>
2014-02-13 10:58:04 -0800

    Working basic http webserver and client

--- helper/http-trial.rkt
+++ helper/http-trial.rkt
@@ -8,55 +8,48 @@
 ;web-server-lib)
 
 
-(define (my-app req)
-  (print-req req)
-  (printf "~~user = ~a\n " (url-user (request-uri req)))
-  (printf "~~host = ~a\n " (url-host (request-uri req)))
+(define (variant-server req)
+  ;(print-req req)
+  ;(printf "~~user = ~a\n " (url-user (request-uri req)))
+  ;(printf "~~host = ~a\n " (url-host (request-uri req)))
   (response/xexpr
    (cond
      [(equal? (request-method req) #"GET")
       `(variant ((value "basic")))]
-;      `(html (head (title "Hello world!"))
-;             (body (p "A GET method!")))]
+
      [(equal? (request-method req) #"POST")
-      `(html (head (title "Hello world!"))
-             (body (p "A POST method!")))])))
+      `(variant ((value "error")))])))
 
-;   ;(read-line req)))
-;   (when r
-;     ; Discard the rest of the header (up to blank line):
-;     (regexp-match #rx"(\r\n|^)\r\n" req)
-;     ; Dispatch:
-;     (let ([xexpr (dispatch (list-ref r 1))])
-;       ; Send reply:
+(serve/servlet variant-server #:port 8080
+               #:servlet-path "/variant")
 
+;;;;;;;;;;;;;;;;
+; DEBUG PRINTS ;
+;;;;;;;;;;;;;;;;
 
-;   `(html (head (title "Hello world!"))
-;          (body (p "Hey out there!")))))  
-
-(define (print-req r)
-  (printf "\n****************\n")
-  (printf "req -")
-  (printf "\n~a\n" r)
-  (printf "method = ~a\n" (request-method r))
-  (printf "t/f ~a\n" (equal? (request-method r) #"GET"))
-  ;(equal? (request-method r) 'GET)
-  (printf "uri = ~a\n" (request-uri r))
-  (printf "headers = ~a\n" (request-headers/raw r))
-  (printf "bindings = ~a\n" (request-bindings/raw-promise r))
-  (printf "postdata = ~a\n" (request-post-data/raw r))
-  (printf "hostip = ~a\n" (request-host-ip r))
-  (printf "host-port = ~a\n" (request-host-port r))
-  (printf "client-ip = ~a\n" (request-client-ip r))
-  (printf "\n****************\n")
-  (printf "url info -\n")
-  (printf "scheme = ~a\n " (url-scheme (request-uri r)))
-  (printf "user = ~a\n " (url-user (request-uri r)))
-  (printf "host = ~a\n " (url-host (request-uri r)))
-  (printf "port = ~a\n " (url-port (request-uri r)))
-  (printf "path-absolute = ~a\n " (url-path-absolute? (request-uri r)))
-  (printf "query = ~a\n " (url-query (request-uri r)))
-  (printf "fragment = ~a\n " (url-fragment (request-uri r))))
+;(define (print-req r)
+;  (printf "\n****************\n")
+;  (printf "req -")
+;  (printf "\n~a\n" r)
+;  (printf "method = ~a\n" (request-method r))
+;  (printf "t/f ~a\n" (equal? (request-method r) #"GET"))
+;  ;(equal? (request-method r) 'GET)
+;  (printf "uri = ~a\n" (request-uri r))
+;  (printf "headers = ~a\n" (request-headers/raw r))
+;  (printf "bindings = ~a\n" (request-bindings/raw-promise r))
+;  (printf "postdata = ~a\n" (request-post-data/raw r))
+;  (printf "hostip = ~a\n" (request-host-ip r))
+;  (printf "host-port = ~a\n" (request-host-port r))
+;  (printf "client-ip = ~a\n" (request-client-ip r))
+;  (printf "\n****************\n")
+;  (printf "url info -\n")
+;  (printf "scheme = ~a\n " (url-scheme (request-uri r)))
+;  (printf "user = ~a\n " (url-user (request-uri r)))
+;  (printf "host = ~a\n " (url-host (request-uri r)))
+;  (printf "port = ~a\n " (url-port (request-uri r)))
+;  (printf "path-absolute = ~a\n " (url-path-absolute? (request-uri r)))
+;  (printf "query = ~a\n " (url-query (request-uri r)))
+;  (printf "fragment = ~a\n " (url-fragment (request-uri r))))
 
 ;   (extract-bindings
 ;    'method
@@ -85,8 +78,6 @@
            (λ (query)
              `(html (body "Hello, World!"))))
 
-(serve/servlet my-app #:port 8080
-               #:servlet-path "/variant")
 
 ;(printf "xexpr->xml\n")
 ;(xexpr->xml '(variant ((value "basic"))))
