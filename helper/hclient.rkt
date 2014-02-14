da6252eaba1c0e99a1a02788a75fe05b6c028d6b
Srikanth Manikarnike <srikanth.m13@gmail.com>
2014-02-13 17:05:39 -0800

    Add testing client code Print values sent from sender

--- helper/hclient.rkt
+++ helper/hclient.rkt
@@ -1,27 +1,23 @@
 #lang racket
 
-(require web-server/servlet
-         web-server/servlet-env
-         xml
-         net/http-client
-         rackunit
-         test-engine/racket-tests)
-
-(define (variant-client-get)
-  (define hc (http-conn-open "127.0.0.1" #:port 8080))
-  (define-values (_ __ port) (http-conn-sendrecv! hc "/variant"
-                       #:method #"GET"))
-  (port->string port))
-
-(define (variant-client-post)
-  (define hc (http-conn-open "127.0.0.1" #:port 8080))
-  (define-values (_ __ port) (http-conn-sendrecv! hc "/variant"
-                       #:method #"POST"))
-  (port->string port))
+(require net/http-client)
 
 (module+ test
-  (check-equal? (variant-client-get) 
-                "<variant value=\"basic\"></variant>")
-  (check-equal? (variant-client-post) 
-                "<variant value=\"error\"></variant>"))
+  (require rackunit))
+
+(define move-req 
+  #"<game><board><row><cell><tile value=\"blank\"></tile></cell><cell><tile value=\"blank\"></tile></cell><cell><tile value=\"hut\"></tile></cell><cell><tile value=\"blank\"></tile></cell><cell><tile value=\"blank\"></tile></cell></row><row><cell><tile value=\"tree\"></tile></cell><cell><tile value=\"blank\"></tile></cell><cell><tile value=\"blank\"></tile></cell><cell><tile value=\"blank\"></tile></cell><cell><tile value=\"blank\"></tile></cell><cell><tile value=\"blank\"></tile></cell></row><row><cell><tile value=\"blank\"></tile></cell><cell><tile value=\"blank\"></tile></cell><cell><tile value=\"blank\"></tile></cell><cell><tile value=\"blank\"></tile></cell><cell><tile value=\"mansion\"></tile></cell><cell><tile value=\"blank\"></tile></cell></row><row><cell><tile value=\"blank\"></tile></cell><cell><tile value=\"blank\"></tile></cell><cell><tile value=\"blank\"></tile></cell><cell><tile value=\"house\"></tile></cell><cell><tile value=\"blank\"></tile></cell><cell><tile value=\"blank\"></tile></cell></row><row><cell><tile value=\"blank\"></tile></cell><cell><tile value=\"blank\"></tile></cell><cell><tile value=\"blank\"></tile></cell><cell><tile value=\"grass\"></tile></cell><cell><tile value=\"blank\"></tile></cell><cell><tile value=\"bush\"></tile></cell></row><row><cell><tile value=\"blank\"></tile></cell><cell><tile value=\"blank\"></tile></cell><cell><tile value=\"blank\"></tile></cell><cell><tile value=\"blank\"></tile></cell><cell><tile value=\"tree\"></tile></cell><cell><tile value=\"blank\"></tile></cell></row></board><current><tile value=\"crystal\"></tile></current><storehouse><tile value=\"crystal\"></tile></storehouse></game>")
+(define (client h p m u d)
+  (define hc (http-conn-open h #:port p))
+  (define-values (_ __ port) (http-conn-sendrecv! hc u
+                       #:method m #:data d))
+  (port->string port))
+
+(client "127.0.0.1" 8080 #"POST" "/move" move-req)
+
+;(module+ test
+;  (check-equal? (client "127.0.0.1" 8080 #"GET" "/variant") 
+;                "<variant value=\"basic\"></variant>")
+;  (check-equal? (client "127.0.0.1" 8080 #"POST" "/variant") 
+;                "<variant value=\"error\"></variant>"))
                 
