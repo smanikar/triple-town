95aea9c1e95c6a6b1b2b3886626c40988b21dc86
Srikanth Manikarnike <srikanth.m13@gmail.com>
2014-02-20 11:17:32 -0800

    Add examples for testing

--- basic/basic-player-definitions.rkt
+++ basic/basic-player-definitions.rkt
@@ -26,18 +26,18 @@
 
 (define b9 
   (list
-   (list (tile 'blank 0 0) (tile 'tree 1 0) (tile 'house 2 0) 
+   (list (tile 'grass 0 0) (tile 'tree 1 0) (tile 'house 2 0) 
          (tile 'house 3 0) (tile 'blank 4 0) (tile 'blank 5 0))
    (list (tile 'castle 0 1) (tile 'castle 1 1) (tile 'blank 2 1) 
          (tile 'mansion 3 1) (tile 'blank 4 1) (tile 'blank 5 1))
    (list (tile 'bush 0 2) (tile 'floating-castle 1 2) (tile 'floating-castle 2 2) 
          (tile 'mansion 3 2) (tile 'blank 4 2) (tile 'blank 5 2))
    (list (tile 'bush 0 3) (tile 'castle 1 3) (tile 'tree 2 3) 
-         (tile 'tree 3 3) (tile 'blank 4 4) (tile 'blank 5 4))
+         (tile 'tree 3 3) (tile 'grass 4 3) (tile 'blank 5 4))
    (list (tile 'blank 0 4) (tile 'blank 1 4) (tile 'blank 2 4)
-         (tile 'blank 3 4) (tile 'blank 4 4) (tile 'blank 5 4))
-   (list (tile 'blank 0 4) (tile 'blank 1 4) (tile 'blank 2 4)
-         (tile 'blank 3 4) (tile 'blank 4 4) (tile 'blank 5 4))))
+         (tile 'grass 3 4) (tile 'blank 4 4) (tile 'blank 5 4))
+   (list (tile 'grass 0 5) (tile 'blank 1 5) (tile 'blank 2 5)
+         (tile 'blank 3 5) (tile 'blank 4 5) (tile 'tree 5 5))))
 
 (define b10 
   (list
@@ -68,3 +68,13 @@
          (tile 'blank 3 4) (tile 'blank 4 4) (tile 'blank 5 4))
    (list (tile 'blank 0 4) (tile 'blank 1 4) (tile 'blank 2 4)
          (tile 'blank 3 4) (tile 'blank 4 4) (tile 'blank 5 4))))
+
+(define t3
+  (list
+   (list (tile 'blank 0 0) (tile 'blank 1 0) (tile 'blank 2 0) (tile 'blank 3 0))
+   (list (tile 'blank 0 1) (tile 'blank 1 1) (tile 'blank 2 1) (tile 'blank 3 1))
+   (list (tile 'blank 0 2) (tile 'grass 1 2) (tile 'grass 2 2) (tile 'blank 3 2))
+   (list (tile 'blank 0 3) (tile 'blank 1 3) (tile 'blank 2 3) (tile 'blank 3 3))))
+
+(define t4 (list (list (tile 'blank 0 0) (tile 'blank 1 0))
+                 (list (tile 'tree 0 1) (tile 'tree 1 1))))
