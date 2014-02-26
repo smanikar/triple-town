c1e648dba80fc8f559df9eb38dc61e72a5535432
Srikanth Manikarnike <srikanth.m13@gmail.com>
2014-02-26 07:51:28 -0700

    Move redundant validate-xml code to helper functions

--- basic/basic-player-server-move.rkt
+++
@@ -1,7 +0,0 @@
-#lang racket
-
-(require web-server/servlet-env
-         "basic-player-functions.rkt")
-
-(serve/servlet server/player #:port 8080
-               #:servlet-regexp (regexp ".*"))
--- basic/basic-player-server-variant.rkt
+++
@@ -1,7 +0,0 @@
-#lang racket
-
-(require web-server/servlet-env
-         "basic-player-functions.rkt")
-
-(serve/servlet variant-server #:port 8080
-              #:servlet-path "/variant")
